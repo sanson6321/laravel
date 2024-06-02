5265e3e2222b45d2b8108a300e541e0bb0489c10
k.kurogi <k.kurogi@trans-it.net>
2024-06-02 19:13:59 +0900

    baseを作成

---
+++ resources/views/layout/base.blade.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="robots" content="noindex">
+    <meta name="viewport" content="width=device-width,initial-scale=1.0">
+    <meta name="format-detection" content="telephone=no">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+    <title>@yield('title'){{ ' | ' . config('app.name') }}</title>
+    <link rel="shortcut icon" href="{{ asset('favicon.ico') }}">
+    <!-- stylesheet -->
+    <link rel="stylesheet" href="{{ FormatAsset::asset('css/style.css') }}">
+    @yield('css')
+    <!-- /stylesheet -->
+    <!-- fontawesome -->
+    <link rel="stylesheet" href="{{ asset('css/all.min.css') }}">
+    <!-- /fontawesome -->
+    <!-- javascript -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <script src="{{ FormatAsset::asset('js/common.js') }}"></script>
+    <!-- /javascript -->
+</head>
+
+<body>
+    @if (session('message_success'))
+        <div id="toast" @class([
+            'bg_green',
+            'session_toast',
+            'toast' => session('message_error'),
+        ])>
+            <p>{{ session()->pull('message_success') }}</p>
+        </div>
+    @endif
+    @if (session('message_error'))
+        <div id="toast" class="bg_red session_toast">
+            <p>{{ session()->pull('message_error') }}</p>
+        </div>
+    @endif
+    @if ($errors->isNotEmpty())
+        <div id="toast" class="bg_red session_toast">
+            @foreach ($errors->all() as $error)
+                <p>{{ $error }}</p>
+            @endforeach
+        </div>
+    @endif
+    <div id="toast" class="bg_red ajax_toast">
+        <p id="message_error"></p>
+    </div>
+    {{-- Tooltip --}}
+    <div class="description">
+    </div>
+    @yield('content')
+    <!-- javascript -->
+    @yield('js')
+    <script>
+        $(function() {
+            console.log('%c' + @json(config('app.name')), "color: #000; font-weight: bold; font-size: 25px;");
+        });
+    </script>
+    <!-- /javascript -->
+</body>
+
+</html>
